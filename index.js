150b6e975b28bc3d37ee13b57757c9cd4cb10531
rafif2112 <m.rafif887@gmail.com>
2026-06-10 20:29:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+const { axios } = require("axios");
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true
+});
+
+app.command("/rafif-dev-ping", async ({ command, ack, respond }) => {
+    const start = Date.now();
+    await ack();
+    const latency = Date.now() - start;
+    await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/dsb-catfact", async ({ command, ack, respond }) => {
+    await ack();
+
+    try {
+        const response = await axios.get("https://catfact.ninja/fact");
+        await respond({ text: `Cat Fact:\n${response.data.fact}` });
+    } catch (err) {
+        await respond({
+            text: "Failed to fetch cat fact. Please try again later."
+        })
+    }
+})
+
+app.command("/dsb-joke", async ({ command, ack, respond }) => {
+    await ack();
+
+    try {
+        const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+        await respond({
+            text: `${response.data.setup} ${response.data.punchline}`
+        });
+    } catch (err) {
+        await respond({ text: "Failed to fetch a joke." });
+    }
+});
+
+(async () => {
+    await app.start();
+    console.log("bot is running!");
+})();
